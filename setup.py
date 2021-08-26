575fe189e14e8e411066c17a83d30af478b3e96a
Brandon Wolfe <Wolfe1@gmail.com>
2021-08-26 15:57:50 -0500

    test

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(name='robotframework-appiumlibrary',
           'decorator >= 3.3.2',
           'robotframework >= 2.6.0',
           'docutils >= 0.8.1',
-          'Appium-Python-Client >= 1.1.0',
+          'Appium-Python-Client==2.0.0b4',
           'selenium >= 4.0.0b4',
           'kitchen >= 1.2.4',
           'six >= 1.10.0'
